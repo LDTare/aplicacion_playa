db51a12173e537961184162a5729a9712280079b
LDTare <joanrandy34@gmail.com>
2022-08-26 03:52:41 -0600

    Indicador circular

--- lib/Screen2.dart
+++ lib/Screen2.dart
@@ -249,10 +249,12 @@ class _cuerpo extends State<cuerpo> with SingleTickerProviderStateMixin{
             ),
 
             Container(
-              height: 50,
+              height: 40,
               width: 250,
               margin:  EdgeInsets.only(left: 25.0, right: MediaQuery.of(context).size.width * 0.4),
               child: TabBar(
+                
+                indicator: CircleTabIndicator(color: Colors.blue, radius: 3),
                 labelColor: Colors.blue,
                 labelStyle: Theme.of(context).textTheme.labelLarge?.copyWith(
                   fontWeight: FontWeight.w500
@@ -345,4 +347,30 @@ class _cuerpo extends State<cuerpo> with SingleTickerProviderStateMixin{
       ),
     );
   }  
+}
+
+
+class CircleTabIndicator extends Decoration {
+  final BoxPainter _painter;
+
+  CircleTabIndicator({required Color color, required double radius}) : _painter = _CirclePainter(color, radius);
+
+  @override
+  BoxPainter createBoxPainter([VoidCallback? onChanged]) => _painter;
+}
+
+class _CirclePainter extends BoxPainter {
+  final Paint _paint;
+  final double radius;
+
+  _CirclePainter(Color color, this.radius)
+      : _paint = Paint()
+          ..color = color
+          ..isAntiAlias = true;
+
+  @override
+  void paint(Canvas canvas, Offset offset, ImageConfiguration cfg) {
+    final Offset circleOffset = offset + Offset(cfg.size!.width / 2, cfg.size!.height - radius);
+    canvas.drawCircle(circleOffset, radius, _paint);
+  }
 }
